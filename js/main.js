d195ff30dac60a17c7d129df8d4193e04ef40484
Thiago-S-O <thiagosoxiii@outlook.com>
2024-04-20 01:05:27 -0300

    botão excluir itens e limpar objeto do localStorage pelo id

--- js/main.js
+++ js/main.js
@@ -26,9 +26,10 @@ form.addEventListener("submit", (evento) => {
         atualizaElemento(itemAtual);
 
         itens[existe.id] = itemAtual; 
+        itens[itens.findIndex(elemento => elemento.id === existe.id)] = itemAtual; 
         // quando atualizar a quantidade de algum objeto, ele vai sobrescrever essa quantidade no localStorage
     } else {
-        itemAtual.id = itens.length;
+        itemAtual.id = itens[itens.length - 1] ? (itens[itens.length - 1]).id + 1 : 0 ;
 
         criarElemento(itemAtual);
 
@@ -51,7 +52,10 @@ function criarElemento(item) {
     numeroItem.innerHTML = item.quantidade;
     numeroItem.dataset.id = item.id;
     novoItem.appendChild(numeroItem);
+    
     novoItem.innerHTML += item.nome;
+
+    novoItem.appendChild(botaoDeleta(item.id));
 
     lista.appendChild(novoItem);
 }
@@ -60,6 +64,25 @@ function atualizaElemento(item) {
     document.querySelector("[data-id='"+item.id+"']").innerHTML = item.quantidade
 }
 
+function botaoDeleta(id) {
+    const elementoBotao = document.createElement('button');
+    elementoBotao.innerText = '❌';
+
+    elementoBotao.addEventListener('click', function() {
+        deletaElemento(this.parentNode, id);
+    });
+
+    return elementoBotao;
+}
+
+function deletaElemento(tag, id) {
+    // localStorage.clear();
+    tag.remove();
+    console.log(id)
+    itens.splice(itens.findIndex(elemento => elemento.id === id), 1);
+
+    localStorage.setItem('itens',JSON.stringify(itens));
+}
 
 /**
  * "evento.preventDefault()"
